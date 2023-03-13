22a587f0482c47dfa326edf197f7d4c296ed17c0
Dima <dimjke-72@mail.ru>
2023-03-13 10:21:41 +0500

    file finalproject changed

--- Program.cs
+++ Program.cs
@@ -40,3 +40,36 @@ void FillNewArray(string[] oldArray, string[] newArray, int lengthLimit)
     }
   }
 }
+
+string? yesNo = "";
+while (yesNo.ToLower() != "yes" && yesNo.ToLower() != "no")
+{
+  Console.Write("Желаете вводить строки вручную? (yes/no): ");
+  yesNo = Console.ReadLine();
+}
+
+string[] arrayOfStrings = new string[]  {};
+
+if (yesNo.ToLower() == "yes") {
+  int n = InputNumbers("Введите количество элементов массива: ");
+  arrayOfStrings = new string[n];
+  for (int i = 0; i < arrayOfStrings.Length; i++)
+  {
+    Console.Write($" Введите {i+1} строку: ");
+    arrayOfStrings[i] = Console.ReadLine();
+  }
+}
+else 
+{
+  arrayOfStrings = new string[]  {"hello", "2", "world", ":-)"};
+}
+
+int lengthLimit = 3;
+
+int numbersItems = CheckArray(arrayOfStrings, lengthLimit);
+
+string[] newArrayOfStrings = new string[numbersItems];
+
+FillNewArray(arrayOfStrings, newArrayOfStrings, lengthLimit);
+
+Console.WriteLine($"{PrintArray(newArrayOfStrings)}");
